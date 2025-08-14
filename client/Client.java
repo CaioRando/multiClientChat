4aea5e52caa2d5d5f3c4e0ea8ff6e605b347e6f5
CaioRando <caiomarostica@outlook.com>
2025-08-14 13:33:19 -0300

    Fixed client bug Client needs to enter username before entering the chat Added is valid username check

--- client/Client.java
+++ client/Client.java
@@ -3,24 +3,54 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Client {
     private static final Logger log = Logger.getLogger(Client.class.getName());
 
+    private static boolean isValidUsername(String username) {
+
+        String regex = "^[A-Za-z0-9_][A-Za-z0-9_\s]{2,15}$";
+        Pattern p = Pattern.compile(regex);
+
+        if(username == null) {
+            return false;
+        }
+
+        Matcher m = p.matcher(username);
+        return m.matches();
+    }
+
+    private static String getUserData(Scanner userInput) {
+        
+        String username = null;
+        while(true) {
+            System.out.printf("Username: ");
+            username = userInput.nextLine().trim();
+
+            if(isValidUsername(username)) {
+                break;
+            } else {
+                System.out.println("Username must have 3-16 valid characters.");
+            }
+        }
+
+        return username;
+    }
     public static void main(String[] args) {
         final String SERVER_ADDRESS = "127.0.0.1"; // localhost or change IP
         final int PORT = 12345;
+        Scanner userInput = new Scanner(System.in);
+        final String username = getUserData(userInput);
 
         try(Socket socket = new Socket(SERVER_ADDRESS, PORT)) {
             log.info("Connected to server " + SERVER_ADDRESS + " PORT " + PORT);
 
-            ServerListener serverListener = new ServerListener(socket);
-            new Thread(serverListener).start();
-            Scanner userInput = new Scanner(System.in);
+            ServerListener serverListener = new ServerListener(socket);            
             PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-
-            System.out.printf("Username: ");
-            final String username = userInput.nextLine();
+            new Thread(serverListener).start();
+            
             writer.println(username);
             while(userInput.hasNextLine()){
                 String message = userInput.nextLine();
